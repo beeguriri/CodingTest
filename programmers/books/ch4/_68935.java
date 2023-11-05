5f89a3cbf092426d8087c23a185edfb56e9941a0
Hyejin Kang <122770696+beeguriri@users.noreply.github.com>
2023-11-05 20:48:37 +0900

    1105 study (#43)
    * ch3. 배열
    
    * ch4. 문자열
    
    * ch5. 재귀
    
    * ch6. 완전탐색

---
+++ programmers/books/ch4/_68935.java
@@ -0,0 +1,16 @@
+package com.study.codingtest.programmers.books.ch4;
+
+public class _68935 {
+
+    public int solution(int n) {
+        int answer = 0;
+        String str = Integer.toString(n, 3);
+        StringBuilder reverse = new StringBuilder();
+        for(int i = str.length()-1; i>=0; i--)
+            reverse.append(str.charAt(i));
+
+        answer = Integer.parseInt(reverse.toString(), 3);
+
+        return answer;
+    }
+}
---
+++ programmers/books/ch4/_81301.java
@@ -0,0 +1,4 @@
+package com.study.codingtest.programmers.books.ch4;
+
+public class _81301 {
+}
